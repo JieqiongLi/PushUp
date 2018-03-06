2b35081e37fa14dc26bb2bbb783728234b1d8066
JieqiongLi <jieqiongli@outlook.com>
2018-03-06 14:10:41 -0500

    Initial commit

---
+++ app/src/main/java/com/test/jq/pushup/MainActivity.java
@@ -0,0 +1,16 @@
+package com.test.jq.pushup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void testUpload(){
+        int i = 1 + 1;
+    }
+}
